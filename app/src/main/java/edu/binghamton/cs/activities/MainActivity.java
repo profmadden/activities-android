68ad0e16b067a51ab9aae1ab6360ccca1b876ca4
Patrick Madden <pmadden@Nostromo-2.local>
2019-03-06 07:43:26 -0500

    Initial commit -- two activities available

---
+++ app/src/main/java/edu/binghamton/cs/activities/MainActivity.java
@@ -0,0 +1,60 @@
+package edu.binghamton.cs.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+// NOTE!  IMPORTANT!
+// Take a look at AndroidManifest.xml
+// We have to list the different activities in the app in
+// that file.  Android scans that file to figure out what
+// sorts of screens can pop up in the app (and they
+// do some crash-testing to make sure each screen can
+// run correctly).
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent secondIntent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(secondIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
